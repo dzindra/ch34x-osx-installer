7278b5a029fb6a29828ce291390ec404c51654b0
Jindra Dolezy <jindrich@dolezy.cz>
2016-04-14 10:04:34 +0200

    Add files

---
+++ pack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pkgbuild --component usbserial.kext/ --scripts scripts --install-location /Library/Extensions --ownership recommended --sign "Developer ID Installer: Jindrich Dolezy (9W7F66BR5D)"  CH34x_Driver.pkg
